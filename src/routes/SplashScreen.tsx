eaedaa998e15f5ef3d88e79212a822e62ca37898
jungDongWook <jdwdonga21@zeroweb.kr>
2024-01-24 09:19:20 +0900

    권한을 위한 Android, ios 작업, SplashScreen 수정

--- src/routes/SplashScreen.tsx
+++ src/routes/SplashScreen.tsx
@@ -10,14 +10,20 @@ class SplashScreen extends Component<SplashScreenProps> {
   timeout = null as null | NodeJS.Timeout;
   
   componentDidMount() {
+    console.log("시작화면")
     this.timeout = setTimeout(() => {
-      this.props.navigation.navigate('Main');
+      this.onNextScreen();
     }, 3000); // 3초 후에 메인 페이지로 이동
   }
   componentWillUnmount(): void {
+    console.log("시작화면 끝")
     if(this.timeout != null){
       clearTimeout(this.timeout);
     }      
+  }
+
+  onNextScreen = () => {
+    this.props.navigation.navigate('Main');
   }
 
   render() {
