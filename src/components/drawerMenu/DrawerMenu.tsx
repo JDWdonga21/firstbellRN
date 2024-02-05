7ec5b5b27fe0becf440bece66970e67e9b6e890d
jungDongWook <jdwdonga21@zeroweb.kr>
2024-02-05 15:40:59 +0900

    드로우 메뉴 : 내용 작성

--- src/components/drawerMenu/DrawerMenu.tsx
+++ src/components/drawerMenu/DrawerMenu.tsx
@@ -1,5 +1,6 @@
 import React, {Component} from "react";
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
+import PlatformTouchable from 'react-native-platform-touchable';
 
 type DrawerMenuProps = {
 
@@ -8,11 +9,99 @@ type DrawerMenuProps = {
 class DrawerMenu extends Component<DrawerMenuProps>{
     render() {
         return(
-            <View style={{flex: 1, padding: 20, backgroundColor: 'gray'}}>
-                <Text>드로우 메뉴</Text>
+            <View style={styles.container}>
+                <View style={styles.header}>
+                    <Text style={styles.titleText}>
+                        메뉴
+                    </Text>
+                </View>
+                <View style={styles.bodys}>                    
+                    <PlatformTouchable
+                        onPress={() => {}}
+                        background={
+                            PlatformTouchable.Ripple('#ff0000', false)
+                        }
+                    >
+                        <View style={{margin: 10, padding: 5}}>
+                            <Text style={styles.bodyText}>
+                                메뉴 1
+                            </Text>
+                        </View>                        
+                    </PlatformTouchable>
+                    <PlatformTouchable
+                        onPress={() => {}}
+                        background={
+                            PlatformTouchable.Ripple('#00ff00', false)
+                        }
+                    >
+                        <View style={{margin: 10, padding: 5}}>
+                            <Text style={styles.bodyText}>
+                                메뉴 2
+                            </Text>
+                        </View>                        
+                    </PlatformTouchable>
+                    <PlatformTouchable
+                        onPress={() => {}}
+                        background={
+                            PlatformTouchable.Ripple('#0000ff', false)
+                        }
+                    >
+                        <View style={{margin: 10, padding: 5}}>
+                            <Text style={styles.bodyText}>
+                                메뉴 3
+                            </Text>
+                        </View>                        
+                    </PlatformTouchable>
+                </View>
+                <View style={styles.footer}>
+                    <View>
+                        <Text style={styles.bodyText}>
+                            하단 텍스트
+                        </Text>
+                    </View>
+                </View>                
             </View>
         )        
     }
 }
+const styles = StyleSheet.create({
+    container: {
+        flex: 1, 
+        padding: 30, 
+        backgroundColor: 'gray'
+    },
+    //
+    header: {
+        flex: 1,
+        margin: 20,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    titleText: {
+        fontSize: 30,
+        fontWeight: 'bold',
+        textAlign: 'center',
+    },
+    //
+    bodys: {
+        flex: 6,
+        margin: 20,        
+        alignItems: 'center',
+    },
+    bodyText: {
+        fontSize: 18,
+        textAlign: 'center'
+    },
+    //
+    footer: {
+        flex: 1,
+        margin: 20,
+        alignItems: 'flex-end',
+    },
+    footerText: {
+        fontSize: 12,
+        textAlign: 'center',
+    }
+})
 
 export default DrawerMenu;
